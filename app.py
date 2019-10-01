6ccb09808a8f50e9dd3b7b3f825cf93fffefe7d0
kmurata798 <kmurata798@gmail.com>
2019-10-01 15:30:07 -0700

    users can see single playlist

--- app.py
+++ app.py
@@ -17,17 +17,15 @@ db = client.Playlister
 
 
 # OUR MOCK ARRAY OF PROJECTS
-playlists = [
-    {'title': 'Great Playlist'},
-    {'title': 'Next Playlist'}
-]
+# playlists = [
+#     {'title': 'Great Playlist'},
+#     {'title': 'Next Playlist'}
+# ]
 
 
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
-    for item in playlists:
-        print(item)
     return render_template('playlists_index.html', playlists=playlists.find())
 
 
@@ -46,13 +44,7 @@ def playlists_submit():
 @app.route('/playlists/new')
 def playlists_new():
     """Create a new playlist."""
-    return render_template('playlists_new.html')
-
-
-@app.route('/playlists/<playlist_id>')
-def playlists_show(playlist_id):
-    """Show a single playlist."""
-    return f'My ID is {playlist_id}'
+    return render_template('playlists_new.html', playlist={}, title='New Playlist')
 
 
 @app.route('/playlists/<playlist_id>')
@@ -60,6 +52,27 @@ def playlists_show(playlist_id):
     """Show a single playlist."""
     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist)
+
+
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
 
 if __name__ == '__main__':
