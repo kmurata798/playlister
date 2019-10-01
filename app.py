b3d592ba87cfbe7f46dbeb2109a47825364ee0f4
kmurata798 <kmurata798@gmail.com>
2019-10-01 11:10:18 -0700

    section 2

--- app.py
+++ app.py
@@ -3,10 +3,25 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# @app.route('/')
+# def index():
+#     """Return homepage."""
+#     return render_template('home.html', msg='Flask is Cool!!')
+
+
+# OUR MOCK ARRAY OF PROJECTS
+playlists = [
+    {'title': 'Cat Videos', 'description': 'Cats acting weird'},
+    {'title': '80\'s Music', 'description': 'Don\'t stop believing!'}
+]
+
+
 @app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
+def playlists_index():
+    """Show all playlists."""
+    for item in playlists:
+        print(item)
+    return render_template('playlists_index.html', playlists=playlists)
 
 
 if __name__ == '__main__':
