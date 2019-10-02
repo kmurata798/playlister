ab3dcac7be5a9226083be540c23ad183b5550e7c
kmurata798 <kmurata798@gmail.com>
2019-10-01 21:45:35 -0700

    pushed it

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from pymongo import MongoClient
 from flask import Flask, render_template, request, redirect, url_for
 from bson.objectid import ObjectId
 import os
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -37,8 +38,10 @@ def playlists_submit():
     playlist = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
-        'videos': request.form.get('videos').split()
+        'videos': request.form.get('videos').split(),
+        'created_at': datetime.now()
     }
+    print(playlist)
     playlist_id = playlists.insert_one(playlist).inserted_id
     return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
